7250a49f9192b01d735adbba08e637da2a0afaa3
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-03 15:45:37 -0300

    projeto inicial

---
+++ src/main/java/br/com/caelum/cursos/CursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosApplication.class, args);
+	}
+
+}
