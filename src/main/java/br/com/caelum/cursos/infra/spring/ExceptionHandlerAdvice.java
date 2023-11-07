8902b7136eda0fa4502e07e699b5c52e1bf0abb1
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-07 15:56:07 -0300

    tratamento de exceptions na api

---
+++ src/main/java/br/com/caelum/cursos/infra/spring/ExceptionHandlerAdvice.java
@@ -0,0 +1,37 @@
+package br.com.caelum.cursos.infra.spring;
+
+import br.com.caelum.cursos.domain.core.RegraDeNegocioException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    record ErroDeValidacao(String campo, String erro){
+        ErroDeValidacao(FieldError field) {
+            this(field.getField(), field.getDefaultMessage());
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErroDeValidacao>> handleValidationError(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors().stream().map(ErroDeValidacao::new).toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public ResponseEntity<String> handleRegraDeNegocioException(RegraDeNegocioException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.internalServerError().body("Ocorreu um erro no servidor: " +ex.getMessage());
+    }
+
+}
